85b721385f0e353e84df90061a3f992d8f4ea418
Drew Pham <drewvpham@gmail.com>
2017-05-24 18:14:44 -0500

    needs work

--- apps/username_validations/models.py
+++ apps/username_validations/models.py
@@ -1,20 +1,20 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib import messages
 
 class UserManager(models.Manager):
-    def isValid(self, form_data):
-        result={'errors':[]}
+    def validCheck(self, form_data):
+        print "Inside isValid method."
+        valid=True
         if len(form_data['username'])<8 or len(form_data['username'])>16:
-            result['errors'].append('Username must be between 8 and 16 characters')
-        return result
+            messages.error(request, "Username is required.")
+            valid=False
+        return valid
 
-
-
-# Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=255)
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
 
-objects = UserManager()
+    objects = UserManager()
 # always required when using a model manager
--- apps/username_validations/views.py
+++ apps/username_validations/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import *
+from .models import User
 
 # Create your views here.
 
 def index(request):
+    print "Inside the index method."
     return render(request, 'username_validations/index.html')
-    pass
+
 
 def create(request):
-    print 'inside the create method'
-    print request.POST
-    is_valid = User.objects.isValid(request.POST)
-    if is_valid['errors']:
-        messages.error(request, is_valid['errors'])
-
-    User.objects.create(username=request.POST['username'])
-    users=User.objects.all()
-    context = {
-    "users": User.objects.all()
-    # select * from Blog
-    }
-    return render(request, 'username_validations/success.html')
+    # print 'inside the create method'
+    #Check if user input is valid
+    # is_valid = User.objects.validCheck(request.POST)
+    # if is_valid:
+        user = User.objects.create(username=request.POST['username'])
+        message="The username name you entered {} is valid. Thank You!"
+        messages.success(request, message.format(user.username))
+        return redirect('/success')
+    # return redirect('/')
 
 def success(request):
+    print "Inside the success method."
+    users=User.objects.all().order_by('-id')
 
-    return render(request, 'username_validations/success.html')
+    context = {
+    "users": users
+    }
+    print context
+
+    return render(request, 'username_validations/success.html', context)
